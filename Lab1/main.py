ae3b5fa3d8f59fac7eb2b13611a6faac9d84d2c7
Egor <sea25042003@gmail.com>
2024-02-02 00:17:22 +0300

    Completed Lab1

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,16 +1,45 @@
 import numpy as np
+
 if __name__ == "__main__":
-    # get user's input for matrix A with given size of n
     n = int(input("Enter the size of the matrix: "))
-    # Now accept user's input for the matrix A
-    def inputMatrix(n):
-        A = []
-        for i in range(n):
-            row = list(map(int, input().split()))
-            A.append(row)
-        return A
-    A = inputMatrix(n)
-    # Now accept vector x with height n
-    x = list(map(int, input().split()))
-    # Multiply two matrices
-    
+    x = np.array(input("Enter the elements of vector x: ").split(), dtype=int)
+    ith = int(input("Enter the i: "))
+
+    print("Enter the elements of matrix A: ")
+    A = []
+    for _ in range(n):
+        row = list(map(int, input().split()))
+        A.append(row)
+
+    A = np.array(A)
+
+    A_inv = np.linalg.inv(A)
+
+    A_altered = A.copy()
+    A_altered[:, ith - 1] = x  # ith-1 because the index starts from 0
+
+    l = A_inv @ x
+    # check if ith column of l is equal to zero
+    if l[ith - 1] == 0:
+        raise Exception("Matrix is not reversible")
+    else:
+        print("Matrix is reversible")
+    pass
+
+    # swap ith element of l to -1
+    l_swapped = l.copy()
+    l_swapped[ith - 1] = -1
+
+    l_dot = -1 / l[ith - 1] * l_swapped
+
+    Q = np.eye(n)
+
+    Q[:, ith - 1] = l_dot
+
+    A_altered_inv = Q @ A_inv
+
+    print("Matrix A:\n", A)
+    print("Reversed matrix A:\n", A_inv)
+    print("Vector x:\n", x)
+    print("Altered matrix A:\n", A_altered)
+    print("Final altered inverted matrix A:\n", A_altered_inv)
